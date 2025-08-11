f7573c12b6c3b569930d45464228dbbcb505ceae
RahulHydra <99813127+HydraRecon@users.noreply.github.com>
2025-08-11 14:12:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,10 @@ go install -v github.com/lc/gau/v2/cmd/gau@latest
 go install -v github.com/tomnomnom/waybackurls@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 go install -v github.com/hakluke/hakrawler@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/haccer/subjack@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/gf@latest
 
 echo "[*] Cloning sqlmap (if you want sqlmap in PATH, add it manually or use pip)"
 if [[ ! -d sqlmap ]]; then
